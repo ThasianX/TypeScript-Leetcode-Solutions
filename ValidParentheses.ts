dfc9afb222219ca54e4f53a2f7656433382f0046
Kevin Li <kevinli29033@gmail.com>
2021-03-21 23:04:37 -0700

    Updated ValidParentheses.ts

--- ValidParentheses.ts
+++ ValidParentheses.ts
@@ -1,35 +1,43 @@
-const parenthesesMappings: Record<string, string> = {
+class Stack<T> {
+  private store: T[] = [];
+
+  push = (val: T) => {
+    this.store.push(val);
+  };
+
+  pop = (): T | undefined => {
+    return this.store.pop();
+  };
+
+  get length() {
+    return this.store.length;
+  }
+}
+
+const closedToOpenParentheses: Record<string, string> = {
   ")": "(",
   "}": "{",
-  "]": "["
-}
+  "]": "[",
+};
 
-class Stack<T> {
-  _store: T[] = [];
-  push(val: T) {
-    this._store.push(val);
-  }
-  pop(): T | undefined {
-    return this._store.pop();
-  }
-  isEmpty(): boolean {
-    return this._store.length === 0;
-  }
-}
+// n: length of `s`
+// Space: O(n) - every character in `s` is an open parentheses
+// Time: O(n) - every character in `s` is an open parentheses
 
 function isValid(s: string): boolean {
-  const stack = new Stack<string>();
-  
-  for(let c of s) {
-    const openingParentheses = parenthesesMappings[c];
-    if(openingParentheses) {
-      if(stack.pop() !== openingParentheses) {
-        return false;
-      }
-    } else {
-      stack.push(c);
+  const parentheses = new Stack<string>();
+
+  for (let c of s) {
+    const openParentheses = closedToOpenParentheses[c];
+    // `c` is an open parentheses
+    if (openParentheses === undefined) {
+      parentheses.push(c);
+      // `c` is a closed parentheses
+    } else if (parentheses.pop() !== openParentheses) {
+      return false;
     }
   }
-  
-  return stack.isEmpty();
-};
+
+  // `s` is valid only if all open parentheses have been properly closed
+  return parentheses.length === 0;
+}
